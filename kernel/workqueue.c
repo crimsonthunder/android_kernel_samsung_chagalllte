40afd1a8e87232d6c71fbbad5b737a9b63b9998e
Tkkg1994 <luca.grifo@outlookc.om>
2015-02-08 04:48:52 +0100

    Makefile edit + workqeue.c

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -1911,7 +1911,7 @@ __acquires(&gcwq->lock)
 	 * While we must be careful to not use "work" after this, the trace
 	 * point will only record its address.
 	 */
-	trace_workqueue_execute_end(work);
+	sec_debug_work_log(work);
 	lock_map_release(&lockdep_map);
 	lock_map_release(&cwq->wq->lockdep_map);
 
