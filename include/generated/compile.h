f1a77cb216f7d7488646e65216cbb0394adf0341
Tkkg1994 <luca.grifo@outlook.com>
2015-02-12 14:26:58 +0100

    Enable KMS and try to fix OC  ksm: Provide support to use deferred timers for scanner thread
    KSM thread to scan pages is getting schedule on definite timeout.
    That wakes up CPU from idle state and hence may affect the power
    consumption. Provide an optional support to use deferred timer
    which suites low-power use-cases.
    
    To enable deferred timers,
    $ echo 1 > /sys/kernel/mm/ksm/deferred_timer
    
    Change-Id: I07fe199f97fe1f72f9a9e1b0b757a3ac533719e8
    Signed-off-by: Chintan Pandya <cpandya@codeaurora.org>

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 10 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#10 SMP PREEMPT Thu Feb 12 14:25:33 CET 2015"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "linux-HP-Pavilion-dm4-Notebook-PC"
+#define LINUX_COMPILER "gcc version 4.9.3 20150113 (prerelease) (crosstool-NG 1.20.0 - Linaro GCC 2015.01) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.4.106-IronKernel_Beta-gb0bd38f-dirty"
