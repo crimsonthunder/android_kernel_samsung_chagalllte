77749cb0189eae5ca7f05e86ec5878f33bfe278d
Tkkg1994 <luca.grifo@outlook.com>
2015-03-12 21:48:06 +0100

    sched: Make scheduler aware of cpu frequency state
    Capacity of a cpu (how much performance it can deliver) is partly
    determined by its frequency (P) state, both current frequency as well
    as max frequency it can reach.  Knowing frequency state of cpus will
    help scheduler optimize various functions such as tracking every
    task's cpu demand and placing tasks on various cpus.
    
    This patch has scheduler registering for cpufreq notifications to
    become aware of cpu's frequency state. Subsequent patches will make
    use of derived information for various purposes, such as task's scaled
    load (cpu demand) accounting and task placement.
    
    Change-Id: I376dffa1e7f3f47d0496cd7e6ef8b5642ab79016
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -299,7 +299,6 @@ __read_mostly int scheduler_running;
  */
 int sysctl_sched_rt_runtime = 950000;
 
-
 /*
  * Maximum possible frequency across all cpus. Task demand and cpu
  * capacity (cpu_power) metrics could be scaled in reference to it.
