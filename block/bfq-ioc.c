15b9a49aff1594bc27da6c8e85895329a95f72d1
Sinisa Sehovic <sinisa.sehovic@gmail.com>
2015-09-17 15:31:32 +0200

    block: introduce the BFQ-v7r8 I/O sched for 3.4

--- block/bfq-ioc.c
+++ block/bfq-ioc.c
@@ -16,8 +16,8 @@
  */
 static inline struct bfq_io_cq *icq_to_bic(struct io_cq *icq)
 {
-	/* bic->icq is the first member, %NULL will convert to %NULL */
-	return container_of(icq, struct bfq_io_cq, icq);
+    /* bic->icq is the first member, %NULL will convert to %NULL */
+    return container_of(icq, struct bfq_io_cq, icq);
 }
 
 /**
@@ -28,9 +28,9 @@ static inline struct bfq_io_cq *icq_to_bic(struct io_cq *icq)
  * Queue lock must be held.
  */
 static inline struct bfq_io_cq *bfq_bic_lookup(struct bfq_data *bfqd,
-					       struct io_context *ioc)
+		           struct io_context *ioc)
 {
-	if (ioc)
-		return icq_to_bic(ioc_lookup_icq(ioc, bfqd->queue));
-	return NULL;
+    if (ioc)
+	return icq_to_bic(ioc_lookup_icq(ioc, bfqd->queue));
+    return NULL;
 }
